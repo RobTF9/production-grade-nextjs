45dd0324156c6a75a6c77400813ae5cfc4334b7b
Rob <robtf9@icloud.com>
2021-11-21 17:59:07 +0000

    feat: authentication set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,15 @@
 import 'reflect-metadata'
 import React from 'react'
 import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import { Provider } from 'next-auth/client'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider session={pageProps}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
